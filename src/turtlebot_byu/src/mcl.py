bab0ff9fdd4b205cc90f4ad76608a8d5424cc2f3
Vallan Sherrod <vallan.sherrod@byu.edu>
2017-10-10 17:03:33 -0600

    Pushed what I had so I can work at home

--- src/turtlebot_byu/src/mcl.py
+++ src/turtlebot_byu/src/mcl.py
@@ -3,19 +3,23 @@ import math
 import numpy as np
 import dynamics
 import stat_filter
+import pdb
 
 def mcl_turtlebot(chi, v_c, w_c, ranges, bearings, landmark_pts, dt, alpha, sigma_r, sigma_phi):
     num_points = len(chi)
-    chi_bar = []
+    num_states = len(chi[0])
+    chi_bar = np.zeros((num_points,num_states))
+    print ("chi_bar: ", chi_bar)
     weights = []
     for i in range(0, num_points):
         v_c_cov = alpha[0]*v_c*v_c + alpha[1]*w_c*w_c
         w_c_cov = alpha[2]*v_c*v_c + alpha[3]*w_c*w_c
-        cow = dynamics.propogate_next_state(chi[i], v_c + stat_filter.noise(v_c_cov), w_c + stat_filter.noise(w_c_cov), dt)
-        chi_bar.append(cow)
+        print("chi_i: ", chi[i])
+        x_next = dynamics.propogate_next_state(np.transpose(chi[i]), v_c + stat_filter.noise(v_c_cov), w_c + stat_filter.noise(w_c_cov), dt)
+        print ("x_next: ", x_next)
+        for j in range(0,num_states):
+            chi_bar[i][j] = x_next[j]
 
-        print ("Monkey chi_bar: ", chi_bar)
-        print ("Cow: ", cow)
         weight = 1
         for j in range(0, len(landmark_pts)):
             f = [ranges[j], bearings[j]]
