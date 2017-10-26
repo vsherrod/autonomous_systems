025f258c34054760e68302cd2872490bcdbc42aa
Vallan Sherrod <yonder@milemark11.com>
2017-10-25 21:44:34 -0600

    Made it handle if it got nans in the measurement

--- src/turtlebot_byu/src/sensor_models.py
+++ src/turtlebot_byu/src/sensor_models.py
@@ -2,6 +2,7 @@
 import math
 import numpy as np
 import stat_filter as st
+import pdb
 
 def inverse_range_sensor_model(mx, x, z, theta_k, true_pos, true_neg, alpha = 1.0, beta = 5.0*np.pi/180.0, z_max = 150):
 
@@ -13,6 +14,10 @@ def inverse_range_sensor_model(mx, x, z, theta_k, true_pos, true_neg, alpha = 1.
         k_val.append(math.fabs(phi-theta_k[i]))
 
     k = k_val.index(min(k_val))
+
+    if np.isnan(z[k][0]):
+        z[k][0] = z_max + 1.0
+
 
     if r > min(z_max, z[k][0] + alpha/2.0) or min(k_val) > beta/2.0:
         return st.log_odds(0.5)
