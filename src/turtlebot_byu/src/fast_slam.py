bd151e2a7770730f6e709683d27cd53c8f3ac092
Vallan Sherrod <yonder@milemark11.com>
2017-11-14 09:43:01 -0700

    More implementation of fast slam

--- src/turtlebot_byu/src/fast_slam.py
+++ src/turtlebot_byu/src/fast_slam.py
@@ -5,15 +5,24 @@ import dynamics
 import stat_filter
 import landmark
 
+def calc_z_hat
+
 def calc_H(landmark, particle):
-        delta = np.array([[landmark.x],[landmark.y]]) - np.array([[particle.x],[particle.y]])
-        q = np.dot(delta.T,delta)[0][0]
-        H = [[math.sqrt(q)*delta[0][0], math.sqrt(q)*delta[1][0]],
-                          [-delta[1][0], delta[0][0]]]
-        H = 1/q*H
+    delta = np.array([[landmark.x],[landmark.y]]) - np.array([[particle.x],[particle.y]])
+    q = np.dot(delta.T,delta)[0][0]
+    H = np.array([[math.sqrt(q)*delta[0][0], math.sqrt(q)*delta[1][0]],
+                        [-delta[1][0], delta[0][0]]])
+    H = 1/q*H
 
-        return H
+    return H, delta, q
 
+def calc_z_hat_and_H(landmark, particle):
+
+    H, delta, q = calc_H(landmark, particle)
+
+    z_hat = np.array([[math.sqrt(q)],[np.arctan2(delta[1],delta[0]) - particle.theta]])
+
+    return z_hat, H
 
 
 
@@ -43,6 +52,29 @@ def fast_slam_known_correspondence_turtlebot(v_c, w_c, ranges, bearing, correspo
             H_inv = numpy.linalg.inv(H)
             ldm.sigma = np.dot(np.dot(H_inv,Q),H_inv.T)
             particles[i].weight = .5
+
+        else:
+            idx = particles[i].seen.index(j)
+
+            ldm = particles[i].landmarks[idx]
+
+            z_hat, H = calc_z_hat_and_H(ldm , particles[i])
+
+            Q = np.dot(np.dot(H,ldm.sigma),H.T) + Q
+            K = np.dot(np.dot(ldm.sigma, H.T), numpy.linalg.inv(Q))
+
+            z = np.array([[ranges], [bearing]])
+
+            mu = np.array([[ldm.x],[ldm.y]]) + np.dot(K, np.array( z - z_hat))
+
+            ldm.x = mu[0][0]
+            ldm.y = mu[1][0]
+
+            ldm.sigma = np.dot(np.eye((2,2))-np.dot(K,H),ldm.sigma)
+
+            particles[i].landmarks[idx] = ldm
+
+            particles[i].weight = 2*np.pi*Q
 
 
 
