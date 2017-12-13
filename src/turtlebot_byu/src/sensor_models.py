bb8f9252efc698341a7dd22f256c02e50caf9feb
Vallan Sherrod <vallan.sherrod@byu.edu>
2017-12-12 20:24:06 -0700

    Got rid of some debugging comments and functions

--- src/turtlebot_byu/src/sensor_models.py
+++ src/turtlebot_byu/src/sensor_models.py
@@ -39,15 +39,15 @@ def inverse_range_sensor_model(mx, x, z, theta_k, true_pos, true_neg, alpha = 1.
     r = math.sqrt((mx[0] - x[0])*(mx[0] - x[0]) + (mx[1] - x[1])*(mx[1] - x[1]))
     phi = dynamics.wrap_angle(np.arctan2(mx[1]-x[1],mx[0]-x[0]) - x[2])
 
-    # k_val = np.absolute(phi-theta_k)
+    k_val = np.absolute(phi-theta_k)
 
     # k_val = []
     # for i in range(len(z)):
     #     k_val.append(math.fabs(phi-theta_k[i]))
 
-    # k = np.argmin(k_val)
+    k = np.argmin(k_val)
 
-    k = potato(phi, theta_k)
+    # k = potato(phi, theta_k)
     # k = cow(phi, theta_k)
 
     #maybe shouldn't be doing this here.  Could be preprocessed
@@ -58,15 +58,15 @@ def inverse_range_sensor_model(mx, x, z, theta_k, true_pos, true_neg, alpha = 1.
 
 
     if r > min(z_max, z[k][0] + alpha/2.0) or theta_k[k] > beta/2.0:
-        print "1st"
+        # print "1st"
         return st.log_odds(0.5)
 
     if z[k][0] < z_max and math.fabs(r - z[k][0]) < alpha/2.0:
-        print "2nd"
+        # print "2nd"
         return st.log_odds(true_pos)
 
     if r <= z[k][0]:
-        print "3rd"
+        # print "3rd"
         return st.log_odds(true_neg)
 
 
